7081867cab4390c16766c1e43647b85943fbe4f9
jeanpereira3 <jeansilv13@gmail.com>
2022-02-22 17:02:44 -0300

    UserRepository, UserResource, Zuul config

--- README.md
+++ README.md
@@ -144,3 +144,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 ```
+
+### 4.5 UserRepository, UserResource, Zuul config
---
+++ hr-user/src/main/java/com/jean/projeto/hruser/resources/UserResource.java
@@ -0,0 +1,35 @@
+package com.jean.projeto.hruser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jean.projeto.hruser.entities.User;
+import com.jean.projeto.hruser.repositories.UserRepository;
+
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> finlById(@PathVariable Long id) {		
+		User obj = userRepository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> finlByEmail(@RequestParam String email) {		
+		User obj = userRepository.findByEmail(email);
+		return ResponseEntity.ok(obj);
+	}
+
+}
